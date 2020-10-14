24b40c83bf4005e3e952af4c94f2caf537c7dde3
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-10-14 16:26:57 +0530

    Update keypair.tf

--- examples/basic/keypair.tf
+++ examples/basic/keypair.tf
@@ -1,5 +1,5 @@
 //Create key pair. Instead of exposing the public key, best practice is to call from file by using file function in Terraform
 resource "aws_key_pair" "myTerraformKeyPair" {
-   key_name = "terraformKey"
+   key_name = "terraformKe"
    public_key = "${file("/home/ubuntu/.ssh/id_rsa.pub")}"
 }
